5522ab4e769def4bc0d42e0c30ed0bbbd94ae757
Reuben Brandt <Reubennbrandt@gmail.com>
2016-02-25 11:47:27 -0800

    a few views and models

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,13 @@
+def nokogiri_ify(url)
+  uri = URI(url)
+  @document = Nokogiri.parse(Net::HTTP.get(uri))
+end
+
+
+def fetch_title(document)
+  @title = document.css('h1').first.inner_text
+end
+
+def fetch_body(document)
+  @body = document.css('p').inner_text
+end
