3565a409e6441237eb9937e1ab87564d9e0101be
Reuben Brandt <Reubennbrandt@gmail.com>
2016-02-25 18:04:21 -0800

    going home for the day

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-
+  bindeListeners();
 });
+
+var bindeListeners = function(){
+  $("#url").on("submit", getWebsite)
+}
+
+var getWebsite = function(e){
+  e.preventDefault();
+
+  $.ajax({
+    method: "post",
+    url: "/users/1/websites",
+    data: $(this).serialize(),
+    dataType: 'json'
+    // this will need to change tomorrow
+  }).done(function(response){
+    var panel = $(".panel-body")
+    $.each(response, function(index, value){
+    setTimeout(function(){
+      panel.show(value)
+      }, 5000)
+    })
+  })
+}
