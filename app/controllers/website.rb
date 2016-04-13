e0769c084a77a795d8213da09576fab11cae75bc
Reuben Brandt <Reubennbrandt@gmail.com>
2016-04-12 19:03:00 -0700

    Recenter Jumbotron

--- app/controllers/website.rb
+++ app/controllers/website.rb
@@ -5,15 +5,10 @@ post "/websites" do
   fetch_title(@document)
   fetch_body(@document)
   if request.xhr?
-      @text_array = array_ify(@body)
-      p @text_array
-      {text_array: @text_array, title: @title}.to_json
-      # erb :"/index"
-  else
-    "Then you don't get to use it"
+    @text_array = array_ify(@body)
+    p @text_array
+    {text_array: @text_array, title: @title}.to_json
+    # erb :"/index"
   end
 end
 
-get "/websites" do
-
-end
