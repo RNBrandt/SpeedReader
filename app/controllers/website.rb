11fbfc86f7a6e4d5c96d49204577f170be70efd5
Reuben Brandt <Reubennbrandt@gmail.com>
2016-07-03 00:27:48 -0700

    To instigate Travis

--- app/controllers/website.rb
+++ app/controllers/website.rb
@@ -5,9 +5,6 @@ post "/websites" do
   fetch_body(@document)
   if request.xhr?
     @text_array = array_ify(@body)
-    p @text_array[25]
-    p @text_array[24]
-    p @text_array[26]
     {text_array: @text_array, title: @title}.to_json
     # erb :"/index"
   end
